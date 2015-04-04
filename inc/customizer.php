cb7f84f12d0aafd58d28a7881e3f163c9eef9483
BenSibley <ben@competethemes.com>
2015-04-04 12:52:57 -0400

    added textarea and multi-check box controls

--- inc/customizer.php
+++ inc/customizer.php
@@ -28,6 +28,43 @@ function ct_founder_add_customizer_content( $wp_customize ) {
 		}
 	}
 
+	// create multi-checkbox/select control
+	class ct_founder_multi_checkbox_control extends WP_Customize_Control {
+		public $type = 'multi-checkbox';
+
+		public function render_content() {
+
+			if ( empty( $this->choices ) )
+				return;
+			?>
+			<label>
+				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<select id="comment-display-control" <?php $this->link(); ?> multiple="multiple" style="height: 100%;">
+					<?php
+					foreach ( $this->choices as $value => $label ) {
+						$selected = ( in_array( $value, $this->value() ) ) ? selected( 1, 1, false ) : '';
+						echo '<option value="' . esc_attr( $value ) . '"' . $selected . '>' . $label . '</option>';
+					}
+					?>
+				</select>
+			</label>
+		<?php }
+	}
+
+	// create textarea control
+	class ct_founder_textarea_control extends WP_Customize_Control {
+		public $type = 'textarea';
+
+		public function render_content() {
+			?>
+			<label>
+				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+				<textarea rows="8" style="width:100%;" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
+			</label>
+		<?php
+		}
+	}
+
 	/***** Logo Upload *****/
 
 	// section
