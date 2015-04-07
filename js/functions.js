4b3c55a42fbdb1490df68deea58a95917c82b9cc
BenSibley <ben@competethemes.com>
2015-04-07 10:07:09 -0400

    added js to open/close child menus

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($){
 
     var toggleNavigation = $('#toggle-navigation');
     var menuPrimary = $('#menu-primary');
+    var toggleDropdown = $('.toggle-dropdown');
 
     toggleNavigation.on('click', openPrimaryMenu);
 
@@ -12,6 +13,38 @@ jQuery(document).ready(function($){
             menuPrimary.removeClass('open');
         } else {
             menuPrimary.addClass('open');
+        }
+    }
+
+    // display the dropdown menus
+    toggleDropdown.on('click', openDropdownMenu);
+
+    function openDropdownMenu() {
+
+        // get the buttons parent (li)
+        var menuItem = $(this).parent();
+
+        // if already opened
+        if( menuItem.hasClass('open') ) {
+
+            // remove open class
+            menuItem.removeClass('open');
+
+            // change screen reader text
+            $(this).children('span').text('open child menu');
+
+            // change aria text
+            $(this).attr('aria-expanded', 'false');
+        } else {
+
+            // add class to open the menu
+            menuItem.addClass('open');
+
+            // change screen reader text
+            $(this).children('span').text('close child menu');
+
+            // change aria text
+            $(this).attr('aria-expanded', 'true');
         }
     }
 });
