29b0bfb7c21b6c2f09b1e2de474dfbce058da4eb
BenSibley <ben@competethemes.com>
2015-04-11 10:11:02 -0400

    removed unnecessary resize function

--- js/build/production.js
+++ js/build/production.js
@@ -83,34 +83,6 @@
 })( window.jQuery || window.Zepto );
 jQuery(document).ready(function($){
 
-    (function($,sr){
-
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        var debounce = function (func, threshold, execAsap) {
-            var timeout;
-
-            return function debounced () {
-                var obj = this, args = arguments;
-                function delayed () {
-                    if (!execAsap)
-                        func.apply(obj, args);
-                    timeout = null;
-                };
-
-                if (timeout)
-                    clearTimeout(timeout);
-                else if (execAsap)
-                    func.apply(obj, args);
-
-                timeout = setTimeout(delayed, threshold || 100);
-            };
-        }
-        // smartresize
-        jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-    })(jQuery,'smartresize');
-
     var siteHeader = $('#site-header');
     var titleContainer = $('#title-container');
     var toggleNavigation = $('#toggle-navigation');
