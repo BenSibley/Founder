6a4eeeea340ab3cdb01b2e65885faad4894cea77
BenSibley <ben@competethemes.com>
2015-04-16 10:56:22 -0400

    post tag markup only shows if there are tags now

--- content/post-tags.php
+++ content/post-tags.php
@@ -2,15 +2,13 @@
 $tags = get_the_tags( $post->ID );
 $output = '';
 if($tags){
-	foreach($tags as $tag) {
-		$output .= '<li><a href="'.get_tag_link( $tag->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts tagged %s", 'founder' ), $tag->name ) ) . '">'.$tag->name.'</a></li>';
-	}
+	echo '<div class="post-tags">';
+		echo '<span>' . __("Tagged in", "founder") . '</span>';
+		echo '<ul>';
+			foreach($tags as $tag) {
+				$output .= '<li><a href="'.get_tag_link( $tag->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts tagged %s", 'founder' ), $tag->name ) ) . '">'.$tag->name.'</a></li>';
+			}
+		echo '</ul>';
+	echo '</div>';
 } 
 ?>
-
-<div class="post-tags">
-	<span><?php _e('Tagged in', 'founder') ?> </span>
-	<ul>
-		<?php echo $output; ?>
-	</ul>
-</div> 
