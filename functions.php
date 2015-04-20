580f91834b6eecf0edfc4e98628d37270a150ca5
BenSibley <ben@competethemes.com>
2015-04-20 11:18:10 -0400

    social icon screen reader text i18n

--- functions.php
+++ functions.php
@@ -367,21 +367,21 @@ if( ! function_exists('ct_founder_social_icons_output') ) {
                     ?>
                     <li>
                         <a class="email" target="_blank" href="mailto:<?php echo antispambot( is_email( ct_founder_get_social_url( $source, $key ) ) ); ?>">
-                            <span class="screen-reader-text">email icon</span>
+                            <span class="screen-reader-text"><?php _e('email icon', 'founder'); ?></span>
                             <i class="fa fa-envelope"></i>
                         </a>
                     </li>
                 <?php } elseif ( $active_site == "flickr" || $active_site == "dribbble" || $active_site == "instagram" || $active_site == "soundcloud" || $active_site == "spotify" || $active_site == "vine" || $active_site == "yahoo" || $active_site == "codepen" || $active_site == "delicious" || $active_site == "stumbleupon" || $active_site == "deviantart" || $active_site == "digg" || $active_site == "hacker-news" || $active_site == "vk" || $active_site == 'weibo' || $active_site == 'tencent-weibo' ) { ?>
                     <li>
                         <a class="<?php echo $active_site; ?>" target="_blank" href="<?php echo esc_url( ct_founder_get_social_url( $source, $key ) ); ?>">
-                            <span class="screen-reader-text"><?php echo $active_site; ?> icon</span>
+                            <span class="screen-reader-text"><?php printf( __('%s icon', 'founder'), $active_site ); ?></span>
                             <i class="fa fa-<?php echo esc_attr( $active_site ); ?>"></i>
                         </a>
                     </li>
                 <?php } else { ?>
                     <li>
                         <a class="<?php echo $active_site; ?>" target="_blank" href="<?php echo esc_url( ct_founder_get_social_url( $source, $key ) ); ?>">
-                            <span class="screen-reader-text"><?php echo $active_site; ?> icon</span>
+	                        <span class="screen-reader-text"><?php printf( __('%s icon', 'founder'), $active_site ); ?></span>
                             <i class="fa fa-<?php echo esc_attr( $active_site ); ?>-square"></i>
                         </a>
                     </li>
