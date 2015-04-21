3e719d8fbc12b08a756952d0de38397b66c2798c
BenSibley <ben@competethemes.com>
2015-04-21 16:44:18 -0400

    content-page had a few errors

--- content-page.php
+++ content-page.php
@@ -1,9 +1,9 @@
 <div <?php post_class(); ?>>
 	<article>
-		<?php ct_founder_featured_image(); ?>
 		<div class='post-header'>
 			<h1 class='post-title'><?php the_title(); ?></h1>
 		</div>
+		<?php ct_founder_featured_image(); ?>
 		<div class="post-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages(array('before' => '<p class="singular-pagination">' . __('Pages:','founder'), 'after' => '</p>', ) ); ?>
