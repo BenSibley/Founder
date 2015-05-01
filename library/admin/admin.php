2611b385023f3786138a07325b8c1fff9bcf5845
BenSibley <ben@competethemes.com>
2015-05-01 09:58:59 -0400

    made hybrid function overridable

--- library/admin/admin.php
+++ library/admin/admin.php
@@ -88,58 +88,60 @@ function hybrid_admin_enqueue_styles( $hook_suffix ) {
  * @param  string $post_type      The name of the post type to get templates for.
  * @return array  $post_templates The array of templates.
  */
-function hybrid_get_post_templates( $post_type = 'post' ) {
-	global $hybrid;
+if( !function_exists( 'hybrid_get_post_templates' ) ) {
+	function hybrid_get_post_templates( $post_type = 'post' ) {
+		global $hybrid;
 
-	/* If templates have already been called, just return them. */
-	if ( !empty( $hybrid->post_templates ) && isset( $hybrid->post_templates[ $post_type ] ) )
+		/* If templates have already been called, just return them. */
+		if ( !empty( $hybrid->post_templates ) && isset( $hybrid->post_templates[ $post_type ] ) )
+			return $hybrid->post_templates[ $post_type ];
+
+		/* Else, set up an empty array to house the templates. */
+		else
+			$hybrid->post_templates = array();
+
+		/* Set up an empty post templates array. */
+		$post_templates = array();
+
+		/* Get the post type object. */
+		$post_type_object = get_post_type_object( $post_type );
+
+		/* Get the theme (parent theme if using a child theme) object. */
+		$theme = wp_get_theme( get_template() );
+
+		/* Get the theme PHP files one level deep. */
+		$files = (array) $theme->get_files( 'php', 1 );
+
+		/* If a child theme is active, get its files and merge with the parent theme files. */
+		if ( is_child_theme() ) {
+			$child       = wp_get_theme();
+			$child_files = (array) $child->get_files( 'php', 1 );
+			$files       = array_merge( $files, $child_files );
+		}
+
+		/* Loop through each of the PHP files and check if they are post templates. */
+		foreach ( $files as $file => $path ) {
+
+			/* Get file data based on the post type singular name (e.g., "Post Template", "Book Template", etc.). */
+			$headers = get_file_data(
+				$path,
+				array(
+					"{$post_type_object->name} Template" => "{$post_type_object->name} Template",
+				)
+			);
+
+			/* Continue loop if the header is empty. */
+			if ( empty( $headers["{$post_type_object->name} Template"] ) )
+				continue;
+
+			/* Add the PHP filename and template name to the array. */
+			$post_templates[ $file ] = $headers["{$post_type_object->name} Template"];
+		}
+
+		/* Add the templates to the global $hybrid object. */
+		$hybrid->post_templates[ $post_type ] = array_flip( $post_templates );
+
+		/* Return array of post templates. */
 		return $hybrid->post_templates[ $post_type ];
-
-	/* Else, set up an empty array to house the templates. */
-	else
-		$hybrid->post_templates = array();
-
-	/* Set up an empty post templates array. */
-	$post_templates = array();
-
-	/* Get the post type object. */
-	$post_type_object = get_post_type_object( $post_type );
-
-	/* Get the theme (parent theme if using a child theme) object. */
-	$theme = wp_get_theme( get_template() );
-
-	/* Get the theme PHP files one level deep. */
-	$files = (array) $theme->get_files( 'php', 1 );
-
-	/* If a child theme is active, get its files and merge with the parent theme files. */
-	if ( is_child_theme() ) {
-		$child       = wp_get_theme();
-		$child_files = (array) $child->get_files( 'php', 1 );
-		$files       = array_merge( $files, $child_files );
 	}
-
-	/* Loop through each of the PHP files and check if they are post templates. */
-	foreach ( $files as $file => $path ) {
-
-		/* Get file data based on the post type singular name (e.g., "Post Template", "Book Template", etc.). */
-		$headers = get_file_data(
-			$path,
-			array( 
-				"{$post_type_object->name} Template" => "{$post_type_object->name} Template",
-			)
-		);
-
-		/* Continue loop if the header is empty. */
-		if ( empty( $headers["{$post_type_object->name} Template"] ) )
-			continue;
-
-		/* Add the PHP filename and template name to the array. */
-		$post_templates[ $file ] = $headers["{$post_type_object->name} Template"];
-	}
-
-	/* Add the templates to the global $hybrid object. */
-	$hybrid->post_templates[ $post_type ] = array_flip( $post_templates );
-
-	/* Return array of post templates. */
-	return $hybrid->post_templates[ $post_type ];
 }
