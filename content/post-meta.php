cd61bb08040132252c49d67b647dffb2ae23c26f
BenSibley <ben@competethemes.com>
2015-09-15 08:51:47 -0400

    removed #post-meta id since it was causing markup validation issues

--- content/post-meta.php
+++ content/post-meta.php
@@ -3,7 +3,7 @@ $date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date('r') ) )
 $author = get_the_author();
 ?>
 
-<div id="post-meta" class="post-meta">
+<div class="post-meta">
 	<p>
 		<?php printf( __('Published %1$s by %2$s', 'founder'), $date, $author ); ?>
 	</p>
