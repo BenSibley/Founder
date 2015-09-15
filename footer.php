8040e77aef86c6e9064878c449a7b4f8b9aab5da
BenSibley <ben@competethemes.com>
2015-09-15 09:13:06 -0400

    added sanitization to footer text

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             $site_url = 'https://www.competethemes.com/founder/';
             $footer_text = sprintf( __( '<a href="%s">Founder WordPress Theme</a> by Compete Themes.', 'founder' ), esc_url( $site_url ) );
             $footer_text = apply_filters( 'ct_founder_footer_text', $footer_text );
-            echo $footer_text;
+            echo wp_kses_post( $footer_text );
         ?>
     </span>
 </footer>
