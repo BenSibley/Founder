32147048b5073b718ce8694b0f327023277293f5
BenSibley <ben@competethemes.com>
2015-10-15 08:07:15 -0400

    added postMessage support to custom CSS

--- js/postMessage.js
+++ js/postMessage.js
@@ -5,8 +5,10 @@
      */
 
     var panel = $('html', window.parent.document);
+    var body = $('body');
     var siteTitle = $('#site-title');
     var tagline = $( '.tagline' );
+    var inlineStyles = $('#ct-founder-style-inline-css');
 
     // Site title
     wp.customize( 'blogname', function( value ) {
@@ -35,6 +37,35 @@
                 link.html('<img class="logo" src="' + to + '" />');
             } else {
                 link.html( panel.find('#customize-control-blogname').find('input').val() );
+            }
+        } );
+    } );
+
+
+    // Custom CSS
+
+    // get current Custom CSS
+    var customCSS = panel.find('#customize-control-custom_css').find('textarea').val();
+
+    // get the CSS in the inline element
+    var allCSS = inlineStyles.text();
+
+    // remove the Custom CSS from the other CSS
+    allCSS = allCSS.replace(customCSS, '');
+
+    // update the CSS in the inline element w/o the custom css
+    inlineStyles.text(allCSS);
+
+    // add custom CSS to its own style element
+    body.append('<style id="style-inline-custom-css" type="text/css">' + customCSS + '</style>');
+
+    // Custom CSS
+    wp.customize( 'custom_css', function( value ) {
+        value.bind( function( to ) {
+            $('#style-inline-custom-css').remove();
+            if ( to != '' ) {
+                to = '<style id="style-inline-custom-css" type="text/css">' + to + '</style>';
+                body.append( to );
             }
         } );
     } );
