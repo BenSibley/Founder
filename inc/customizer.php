997c96d43f0fdd998abda3c730cd46d063a0c74f
BenSibley <ben@competethemes.com>
2015-10-15 07:55:40 -0400

    added postMessage for Logo

--- inc/customizer.php
+++ inc/customizer.php
@@ -134,7 +134,8 @@ function ct_founder_add_customizer_content( $wp_customize ) {
 		'default'           => '',
 		'type'              => 'theme_mod',
 		'capability'        => 'edit_theme_options',
-		'sanitize_callback' => 'esc_url_raw'
+		'sanitize_callback' => 'esc_url_raw',
+		'transport'         => 'postMessage'
 	) );
 	// control
 	$wp_customize->add_control( new WP_Customize_Image_Control(
--- js/postMessage.js
+++ js/postMessage.js
@@ -4,12 +4,15 @@
      * Functions for utilizing the postMessage transport setting
      */
 
+    var panel = $('html', window.parent.document);
+    var siteTitle = $('#site-title');
+
     // Site title
     wp.customize( 'blogname', function( value ) {
         value.bind( function( to ) {
             // if there is a logo, don't replace it
             if( $('.site-title').find('img').length == 0 ) {
-                $( '.site-title a' ).text( to );
+                $( '.site-title' ).text( to );
             }
         } );
     } );
@@ -21,6 +24,18 @@
                 $('#title-container').append('<p class="tagline"></p>');
             }
             tagline.text( to );
+        } );
+    } );
+
+    // Logo
+    wp.customize( 'logo_upload', function( value ) {
+        value.bind( function( to ) {
+            var link = siteTitle.children('a');
+            if ( to != '' ) {
+                link.html('<img class="logo" src="' + to + '" />');
+            } else {
+                link.html( panel.find('#customize-control-blogname').find('input').val() );
+            }
         } );
     } );
 
