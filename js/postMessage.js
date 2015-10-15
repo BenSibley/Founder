c60b71acc57b34ded5e015bf6a89d528a46f570e
BenSibley <ben@competethemes.com>
2015-10-15 07:57:16 -0400

    refactoring

--- js/postMessage.js
+++ js/postMessage.js
@@ -6,20 +6,20 @@
 
     var panel = $('html', window.parent.document);
     var siteTitle = $('#site-title');
+    var tagline = $( '.tagline' );
 
     // Site title
     wp.customize( 'blogname', function( value ) {
         value.bind( function( to ) {
             // if there is a logo, don't replace it
-            if( $('.site-title').find('img').length == 0 ) {
-                $( '.site-title' ).text( to );
+            if( siteTitle.find('img').length == 0 ) {
+                siteTitle.children('a').text( to );
             }
         } );
     } );
     // Tagline
     wp.customize( 'blogdescription', function( value ) {
         value.bind( function( to ) {
-            var tagline = $( '.tagline' );
             if ( tagline.length == 0 ) {
                 $('#title-container').append('<p class="tagline"></p>');
             }
