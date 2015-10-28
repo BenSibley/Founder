015ebb7b322fcf67e4becb132ee4d9953fe23a36
BenSibley <ben@competethemes.com>
2015-10-28 09:39:05 -0400

    added 'for' attribute to all comment form labels

--- functions.php
+++ functions.php
@@ -121,21 +121,21 @@ if( ! function_exists( 'founder_update_fields' ) ) {
 
         $fields['author'] =
             '<p class="comment-form-author">
-	            <label>' . __( "Name", "founder" ) . $label . '</label>
+	            <label for="author">' . __( "Name", "founder" ) . $label . '</label>
 	            <input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
             '" size="30" ' . $aria_req . ' />
 	        </p>';
 
         $fields['email'] =
             '<p class="comment-form-email">
-	            <label>' . __( "Email", "founder" ) . $label . '</label>
+	            <label for="email">' . __( "Email", "founder" ) . $label . '</label>
 	            <input id="email" name="email" type="email" value="' . esc_attr( $commenter['comment_author_email'] ) .
             '" size="30" ' . $aria_req . ' />
 	        </p>';
 
         $fields['url'] =
             '<p class="comment-form-url">
-	            <label>' . __( "Website", "founder" ) . '</label>
+	            <label for="url">' . __( "Website", "founder" ) . '</label>
 	            <input id="url" name="url" type="url" value="' . esc_attr( $commenter['comment_author_url'] ) .
             '" size="30" />
 	            </p>';
@@ -150,7 +150,7 @@ if( ! function_exists( 'founder_update_comment_field' ) ) {
 
         $comment_field =
             '<p class="comment-form-comment">
-	            <label>' . __( "Comment", "founder" ) . '</label>
+	            <label for="comment">' . __( "Comment", "founder" ) . '</label>
 	            <textarea required id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
 	        </p>';
 
