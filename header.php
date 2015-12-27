5ec73d3ab35930656ecb09533a967990c884a415
BenSibley <ben@competethemes.com>
2015-12-27 11:32:58 -0500

    added support for Yoast breadcrumbs

--- header.php
+++ header.php
@@ -33,4 +33,7 @@
 			<?php get_sidebar( 'primary' ); ?>
 			<?php do_action( 'before_main' ); ?>
 			<section id="main" class="main" role="main">
-				<?php do_action( 'main_top' ); ?>
+				<?php do_action( 'main_top' );
+				if ( function_exists( 'yoast_breadcrumb' ) ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
+				}
