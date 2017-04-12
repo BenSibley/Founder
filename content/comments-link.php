b957dc339e90b8f7dae9b9ee13d0e0c6941e6d81
BenSibley <ben@competethemes.com>
2017-04-12 11:35:50 -0400

    clarifying translation strings

--- content/comments-link.php
+++ content/comments-link.php
@@ -2,10 +2,10 @@
 	<i class="fa fa-comment" title="<?php esc_attr_e( 'comment icon', 'founder' ); ?>"></i>
 	<?php
 	if ( ! comments_open() && get_comments_number() < 1 ) :
-		comments_number( __( 'Comments closed', 'founder' ), __( 'One Comment', 'founder' ), __( '% Comments', 'founder' ) );
+		comments_number( __( 'Comments closed', 'founder' ), __( 'One Comment', 'founder' ), _x( '% Comments', 'noun: 5 comments', 'founder' ) );
 	else :
 		echo '<a href="' . esc_url( get_comments_link() ) . '">';
-		comments_number( __( 'Leave a Comment', 'founder' ), __( 'One Comment', 'founder' ), __( '% Comments', 'founder' ) );
+		comments_number( __( 'Leave a Comment', 'founder' ), __( 'One Comment', 'founder' ), _x( '% Comments', 'noun: 5 comments', 'founder' ) );
 		echo '</a>';
 	endif;
 	?>
