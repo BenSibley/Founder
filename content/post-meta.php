d6adc4cb8dd7b3a2270375fd8844c4cf851a28de
BenSibley <ben@competethemes.com>
2017-10-30 08:01:22 -0400

    added translator note for clarity

--- content/post-meta.php
+++ content/post-meta.php
@@ -5,6 +5,8 @@ $author = get_the_author();
 
 <div class="post-meta">
 	<p>
-		<?php printf( esc_html__( 'Published %1$s by %2$s', 'founder' ), $date, $author ); ?>
+		<?php
+		// translators: Published "date" by "author" 
+		printf( esc_html__( 'Published %1$s by %2$s', 'founder' ), $date, $author ); ?>
 	</p>
 </div>
