b1f6048220e57d4648f03d87275c5bb57b25beb7
BenSibley <ben@competethemes.com>
2018-10-05 10:38:48 -0400

    missing translation

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -4,7 +4,7 @@
 	$widget_count = count( $widgets['primary'] );
 	?>
 	<aside class="sidebar sidebar-primary" id="sidebar-primary" role="complementary">
-		<h1 class="screen-reader-text">Sidebar</h1>
+		<h1 class="screen-reader-text"><?php esc_html_e('Sidebar', 'founder'); ?></h1>
 		<button id="toggle-sidebar" class="toggle-sidebar" name="toggle-sidebar" aria-expanded="false">
 			<span class="screen-reader-text"><?php _e( 'open sidebar', 'founder' ); ?></span>
 			<i class="fas fa-angle-down" title="<?php _e( 'sidebar icon', 'founder' ); ?>" aria-hidden="true"></i>
