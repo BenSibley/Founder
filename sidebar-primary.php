e28328ffce5ea1e8c85693ff86ac912dc3ee91c1
BenSibley <ben@competethemes.com>
2018-10-05 10:39:36 -0400

    missing escaping

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -6,11 +6,11 @@
 	<aside class="sidebar sidebar-primary" id="sidebar-primary" role="complementary">
 		<h1 class="screen-reader-text"><?php esc_html_e('Sidebar', 'founder'); ?></h1>
 		<button id="toggle-sidebar" class="toggle-sidebar" name="toggle-sidebar" aria-expanded="false">
-			<span class="screen-reader-text"><?php _e( 'open sidebar', 'founder' ); ?></span>
-			<i class="fas fa-angle-down" title="<?php _e( 'sidebar icon', 'founder' ); ?>" aria-hidden="true"></i>
+			<span class="screen-reader-text"><?php esc_html_e( 'open sidebar', 'founder' ); ?></span>
+			<i class="fas fa-angle-down" title="<?php esc_html_e( 'sidebar icon', 'founder' ); ?>" aria-hidden="true"></i>
 		</button>
 		<div id="sidebar-primary-content" class="sidebar-primary-content">
-			<div id="sidebar-primary-widgets" class="sidebar-primary-widgets active-<?php echo $widget_count; ?>">
+			<div id="sidebar-primary-widgets" class="sidebar-primary-widgets active-<?php echo absint($widget_count); ?>">
 				<?php dynamic_sidebar( 'primary' ); ?>
 			</div>
 		</div>
