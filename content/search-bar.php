b80a3d4c635324f9cfd179565668159749510147
BenSibley <ben@competethemes.com>
2019-11-19 19:09:13 -0500

    missing escape

--- content/search-bar.php
+++ content/search-bar.php
@@ -9,7 +9,7 @@ if ( get_theme_mod( 'search_bar' ) != 'show' ) {
 		<i class="fas fa-search"></i>
 	</button>
 	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<label class="screen-reader-text"><?php _e( 'Search', 'founder' ); ?></label>
+		<label class="screen-reader-text"><?php esc_html_e( 'Search', 'founder' ); ?></label>
 		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Search...', 'founder' ); ?>" value="" name="s"
 		       title="<?php esc_attr_e( 'Search for:', 'founder' ); ?>" tabindex="-1"/>
 	</form>
