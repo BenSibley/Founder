a6ac59fc6ffd8a653768a6b63259625c7fb09b58
BenSibley <ben@competethemes.com>
2020-07-29 13:46:13 -0400

    stopped adding read more link to RSS feed

--- functions.php
+++ functions.php
@@ -228,6 +228,9 @@ add_action( 'comment_form_defaults', 'ct_founder_remove_comments_notes_after' );
 
 if ( ! function_exists( 'ct_founder_filter_read_more_link' ) ) {
 	function ct_founder_filter_read_more_link( $custom = false ) {
+		if ( is_feed() ) {
+			return;
+		}
 		global $post;
 		$ismore             = strpos( $post->post_content, '<!--more-->' );
 		$read_more_text     = get_theme_mod( 'read_more_text' );
